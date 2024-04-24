63fc280b6d7a5ad6622727ced1e445fda6942831
DeividsonOmedio <deividsonomedio@outlook.com>
2024-04-24 18:28:02 -0300

    projeto adicionado

---
+++ src/main/java/toDoList/entity/Todo.java
@@ -0,0 +1,17 @@
+package toDoList.entity;
+
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+}
---
+++ src/main/java/toDoList/service/TodoService.java
@@ -0,0 +1,36 @@
+package toDoList.service;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import toDoList.entity.Todo;
+import toDoList.repository.TodoRepository;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+         todoRepository.save(todo);
+         return list();
+    }
+
+    public List<Todo> list(){
+        Sort sort = Sort.by(Sort.Order.desc("prioridade"), Sort.Order.asc("nome"));
+        return todoRepository.findAll(sort);
+    }
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
